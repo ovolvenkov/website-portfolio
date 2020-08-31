6ee2c32a9ba520635567361d5208ed0e1ad0587d
ovolvenkov <ovolvenkov@gmail.com>
2020-08-31 21:53:16 +0300

    make progressbar

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
+const progress = document.querySelectorAll('.progress');
 
-document.addEventListener('scroll', (e)=>{
-	if(pageYOffset >= 800){
-		let progres = document.querySelector('.progress');
-		progres.classList.add('.progress:after')
-	}
+const dataProgress = {
+	'HTML/CSS': 80,
+	'JAVASCRIPT': 60,
+	'PHP': 87,
+	'PHYTON': 75,
+	'RUBY': 40,
+	'JAVA': 28,
+}
+
+
+
+document.addEventListener('scroll',(e)=>{
+		if (document.querySelector('.skills').getBoundingClientRect().y < 200 ) {
+				for (const elem of progress) {
+					let str = elem.parentNode.innerText;
+					console.log(str)
+					elem.style.width = `${dataProgress[str]}%`
+				}
+		}
 })
 
-$(window).scroll(function() { 
- var scrollPercent = 100 * $(window).scrollTop() / ($(document).height() - $(window).height()); 
- $('.bar__long').css('width', scrollPercent + "%"); 
-});
