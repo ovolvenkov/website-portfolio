c40bfdcb90ea34ffb0329b71419456f466079a19
ovolvenkov <ovolvenkov@gmail.com>
2020-09-01 21:07:01 +0300

    make background and scroll top

--- script.js
+++ script.js
@@ -1,23 +1,51 @@
-const progress = document.querySelectorAll('.progress');
 
-const dataProgress = {
+function progressBar(){
+	const progress = document.querySelectorAll('.progress');
+
+	const dataProgress = {
 	'HTML/CSS': 80,
-	'JAVASCRIPT': 60,
-	'PHP': 87,
-	'PHYTON': 75,
+	'JAVASCRIPT': 90,
+	'PHP': 17,
+	'PHYTON': 35,
 	'RUBY': 40,
-	'JAVA': 28,
+	'JAVA': 99,
+	}
+
+	document.addEventListener('scroll',(e)=>{
+		if (document.querySelector('.skills').getBoundingClientRect().y < 400 ) {
+				for (const elem of progress) {
+					let str = elem.parentNode.innerText;
+					elem.style.width = `${dataProgress[str]}%`
+				}
+		}
+	})
+}
+
+progressBar();
+
+function smoothScrollingTop() {
+
+let scrollBtn = document.querySelector('.scrollBtn');
+
+document.addEventListener('scroll', ()=>{
+	if (window.pageYOffset >= 500) {
+		scrollBtn.style.opacity = '1';
+		//scrollBtn.style.display = 'block';
+	} else {
+		scrollBtn.style.opacity = '0';
+		//scrollBtn.style.display = 'none';
+	}
+
+})
+
+scrollBtn.addEventListener('click', (e=>{
+	if(window.pageYOffset >= 500){document.querySelector('.header').scrollIntoView({ behavior: 'smooth' })}
+}))
+
+	//document.querySelector('.expirience_header').onclick = function(e){
+		//document.querySelector('.header').scrollIntoView({ behavior: 'smooth' })
 }
 
 
 
-document.addEventListener('scroll',(e)=>{
-		if (document.querySelector('.skills').getBoundingClientRect().y < 200 ) {
-				for (const elem of progress) {
-					let str = elem.parentNode.innerText;
-					console.log(str)
-					elem.style.width = `${dataProgress[str]}%`
-				}
-		}
-})
-
+smoothScrollingTop()
